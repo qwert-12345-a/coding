c84f9174298e415271dd83a59cdf1dff956317fa
Furitale <2208544961@qq.com>
2025-09-28 20:15:48 +0800

    fix: solve issue,update solution.cpp and README.md

--- solution.cpp
+++ solution.cpp
@@ -1,6 +1,32 @@
+
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
+        auto cmp = [](ListNode* a, ListNode* b) { return a->val > b->val; };
+        priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> pq(cmp);
+        for (auto node : lists) {
+            if (node) pq.push(node);
+        }
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        while (!pq.empty()) {
+            ListNode* node = pq.top(); pq.pop();
+            tail->next = node;
+            tail = tail->next;
+            if (node->next) pq.push(node->next);
+        }
+        return dummy.next;
     }
 };
